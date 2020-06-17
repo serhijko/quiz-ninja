7016cbb3d76e756e9b81cf6e86497f2ce11f9208
serhijko <sp_by@tut.by>
2020-06-17 15:25:01 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+﻿import { view, game } from './quiz.js';
+
+const url = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/questions.json';
+
+fetch(url)
+	.then(res => res.json())
+	.then(quiz => {
+		view.start.addEventListener('click', () => game.start(quiz.questions), false);
+		view.response.addEventListener('click', (event) => game.check(event), false);
+});
