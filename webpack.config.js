d4b093e51b8e960d5704a589145e4e57b02cfed6
serhijko <sp_by@tut.by>
2020-06-17 15:43:19 -0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,14 @@ module.exports = {
 	
 	module: {
 		rules: [
-			test: /\.js$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					presets: ['env']
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
 				}
 			}
 		]
